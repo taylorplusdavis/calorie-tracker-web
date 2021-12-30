881aafd56603c3aae611e5c04eddcf3899d70198
Parshva <runwalparshva3@gmail.com>
2021-12-30 22:59:51 +0530

    renamed navbar to sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,26 +2,13 @@ import {
   BellIcon,
   HeartIcon,
   HomeIcon,
-  UserCircleIcon,
   ViewGridIcon,
 } from "@heroicons/react/solid";
 import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
-function Navbar() {
+function Sidebar() {
   const [button, setButton] = useState("home");
-
-  const navText = {
-    hidden: {
-      opacity: 0,
-    },
-    show: {
-      opacity: 1,
-    },
-    exit: {
-      opacity: 0,
-    },
-  };
 
   return (
     <div className="flex flex-col w-24 bg-slate-700 h-screen items-center">
@@ -31,6 +18,7 @@ function Navbar() {
           onClick={() => setButton("home")}
           className="text-slate-100 p-8 glow cursor-pointer mb-20"
         />
+
         <div className="buttonContainer -rotate-90 bg-slate-600 text-center p-2 cursor-default rounded-full w-[12rem] self-center">
           <AnimatePresence exitBeforeEnter>
             <motion.p
@@ -76,4 +64,4 @@ function Navbar() {
   );
 }
 
-export default Navbar;
+export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import Head from "next/head";
-import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
     <div className="flex bg-slate-100">
-      <Head>
-        <title>Calorie Tracker</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
+      <Sidebar />
     </div>
   );
 }
