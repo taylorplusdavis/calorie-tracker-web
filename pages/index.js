d153118ee45ebb5612795460849c23de18faa722
taylorplusdavis <taylor.p.d94@hotmail.com>
2021-12-30 11:30:22 -0600

    fix sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from "next/head";
-import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex bg-slate-100">
+    <div className="flex bg-slate-100 h-screen">
       <Head>
         <title>Calorie Tracker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
+      <Sidebar />
     </div>
   );
 }
